691637cb496cafa0f1d17376fcccceae3a6188df
Aungkokolin1997 <lin@quartile.co>
2026-06-30 08:08:51 +0000

    [IMP] template_content_swapper: add summary

--- template_content_swapper/__manifest__.py
+++ template_content_swapper/__manifest__.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Template Content Swapper",
+    "summary": "Replace QWeb view elements without custom modules",
     "version": "17.0.1.0.0",
     "author": "Quartile, Odoo Community Association (OCA)",
     "license": "AGPL-3",
